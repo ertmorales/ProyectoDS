19eff7f31e2ac361c6eb06ab5f04ea606ed89c70
Robert L <robertmorales181@gmail.com>
2020-04-09 09:59:00 -0600

    Se modificaron los procesos para insertar de cada tabla para hacer mas segura la transferencia de datos, se halló que en proceso de migracion  o sincronizacion los datos a veces se insertaban completos y otras veces no, tambien se corrigió el Id como filtro para que los datos no se dupliquen, en agunas tablas no se hizo este cambio

--- Apis/src/InsertFunctions/insert_s_doc_t_tra.js
+++ Apis/src/InsertFunctions/insert_s_doc_t_tra.js
@@ -3,60 +3,133 @@ const pool = require("../database/database");
 const fs = require("fs");
 const { generalDir, dirSaveFile } = require("../Global/routesfilesDirs");
 
-async function insert_s_doc_t_tra(name, callback) {
+async function rowValues(name, callback) {
     if (!name) {
         callback(1);
         return;
     } else {
-        //le archivo
-        fs.readFile(generalDir + dirSaveFile.s_doc_t_tra + name, async function(err, text) {
-            if (err) {
-                callback(2);
-                return;
-            } else {
-                const datos = JSON.parse(text);
-                var values = [];
-                datos.forEach(async row => {
-                    await pool.query("select Id from pos.s_doc_t_tra where Id = ?", [row.Id], async function(err, result) {
-                        if (err) {
-                            callback(3);
-                            return;
-                        } else {
-                            if (Object.entries(result).length === 0) {
-                                //console.log(row);
-                                values.push(
-                                    [
-                                        row.Tipo_Documento,
-                                        row.Serie_Documento,
-                                        row.Empresa,
-                                        row.Tipo_Transaccion
-                                    ]
-                                )
-                            }
-                        }
-                    });
-                });
+        if (name != "No data available in table") {
+            fs.readFile(generalDir + dirSaveFile.s_doc_t_tra + name, async function(err, text) {
+                if (err) {
+                    callback(2);
+                    return;
+                } else {
+                    //si el archivo existe, inserta en la db 
+                    const datos = JSON.parse(text);
+                    var values = [];
 
-                await pool.query(
-                    "INSERT INTO pos.s_doc_t_tra" +
-                    "(Tipo_Documento," +
-                    "Serie_Documento," +
-                    "Empresa," +
-                    "Tipo_Transaccion)" +
-                    "VALUES ?", [values],
-                    async function(err, result) {
-                        if (err) {
-                            callback(4);
-                            return;
-                        } else {
-                            callback(201);
-                            return;
-                        }
+                    const con = Object.entries(datos).length - 1;
+
+                    var indexDatos = 0;
+
+                    datos.forEach(async row => {
+
+                        await pool.query("select Id from pos.s_doc_t_tra where Tipo_Documento = ?", [row.Tipo_Documento],
+                            function(err, result) {
+
+                                //Proceso en consola
+                                console.group("Procesando...")
+                                console.log("Tabla s_doc_t_tra:")
+                                console.log("Transacción " + indexDatos + " de " + con);
+                                console.table(row)
+                                console.groupEnd("Procesando...")
+
+                                if (err) {
+                                    callback(3);
+                                    return;
+                                } else {
+
+                                    if (Object.entries(result).length === 0) {
+
+                                        values.push(
+                                            [
+                                                row.Tipo_Documento,
+                                                row.Serie_Documento,
+                                                row.Empresa,
+                                                row.Tipo_Transaccion
+                                            ]
+                                        )
+                                    }
+
+                                    if (indexDatos === con) {
+                                        callback(values);
+                                    }
+
+                                    indexDatos++
+
+                                }
+                            });
+                    });
+                }
+            });
+        } else {
+            callback(6);
+            return;
+        }
+    }
+
+}
+
+
+async function insertToDB(values, callback) {
+    if (values === 1) {
+        callback(values);
+        return;
+    }
+    if (values === 2) {
+        callback(values);
+        return;
+    }
+    if (values === 3) {
+        callback(values);
+        return;
+    }
+    if (values === 6) {
+        callback(values);
+        return;
+    }
+    if (values != 1 && values != 2 && values != 3 && values != 6) {
+        if (Object.entries(values).length === 0) {
+            callback(5);
+            return;
+        } else {
+
+            await pool.query(
+                "INSERT INTO pos.s_doc_t_tra" +
+                "(Tipo_Documento," +
+                "Serie_Documento," +
+                "Empresa," +
+                "Tipo_Transaccion)" +
+                "VALUES ?", [values],
+                async function(err, result) {
+                    if (err) {
+                        callback(4);
+                        return;
+                    } else {
+                        callback(201);
+                        return;
                     }
-                );
-            }
-        });
+                }
+            );
+        }
     }
 }
+
+async function insert_s_doc_t_tra(name, callback) {
+    await rowValues(name, async function(resolve) {
+        if (resolve) {
+            await insertToDB(resolve, async function(resolve) {
+                if (resolve) {
+                    callback(resolve);
+                } else {
+                    callback(8);
+                }
+            });
+        } else {
+            callback(7);
+        }
+    });
+}
+
 
 module.exports = insert_s_doc_t_tra;
