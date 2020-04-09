c295d72f86932144605145b262111e802a47e082
Robert L <robertmorales181@gmail.com>
2020-04-09 10:39:21 -0600

    renombre de acrpetas

--- Apis/src/services/dateNow.js
+++
@@ -1,9 +0,0 @@
-"use strict "
-const moment = require("moment");
-
-function fechaCreacion() {
-    let ahora = moment();
-    return ahora.format('YYYY-MM-DD-HH-mm');
-}
-
-module.exports = fechaCreacion;
